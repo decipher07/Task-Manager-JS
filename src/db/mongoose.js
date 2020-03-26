eb9fbbb3f6da18294e1e0fb90813f5f3ad812d68
Rishabh Bhandari <41220684+RishabhKodes@users.noreply.github.com>
2020-03-26 17:14:46 +0530

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require ('validator')
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect('mongodb://localhost:27017/notes', {
     useNewUrlParser: true ,
     useCreateIndex: true ,
     useFindAndModify: false
